2c9822c809253aa7f2f0374dd0499838d1c2cd9d
Jayanth Chornur Srinivasa <chornur.srinivasa.jayanth@volvo.com>
2017-06-02 07:25:09 +0530

    Examples for Function types

--- app.js
+++ app.js
@@ -3,3 +3,34 @@ var b = 2;
 var c = a+b;
 
 console.log(a+b); // console.log in browser and nodeJS
+
+// function Statement
+function greet() {
+  console.log("Hello NODEJS");
+};
+greet();
+
+// function Expression
+
+var greetKannada = function () {
+  console.log("Namaskara NodeJS");
+};
+greetKannada();
+
+//first call functions
+
+function greetBoth (cb, cb2) {
+  console.log("--- Greeting both !! ----")
+  cb2();
+  cb();
+};
+
+greetBoth(greet , greetKannada);
+
+// using function expression on the fly.
+
+greetBoth(function () {
+  console.log("Hola NODEJS");
+} , function () {
+  console.log("Moshi Moshi NODEJS");
+});
