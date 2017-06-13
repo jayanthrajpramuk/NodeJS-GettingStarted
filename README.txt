ca4a070825772ed893e22e2be7722bb0eeb0f653
Jayanth Chornur Srinivasa <chornur.srinivasa.jayanth@volvo.com>
2017-06-13 13:18:59 +0530

    Code to get port from environment variables

--- README.txt
+++ README.txt
@@ -1,3 +1,5 @@
 Keeping track of NODEJS components via Git
 
 ES6 practices.
+
+^ , ~ some of the features which are present in NPM. Check out those.
--- app.js
+++ app.js
@@ -5,9 +5,13 @@ var port = 5000;
 
 console.log("Before listening to port");
 
+// start and listen to the port mentioned
 app.listen(port , function () {
   console.log("Hello Node in the Port!!", port);
 });
 
 console.log("After listeninf to port");
+
+console.log(process.env.PORT) // if set , will get the value
+console.log(process.env.OS) // if set , will get the value : Windows_NT
 
