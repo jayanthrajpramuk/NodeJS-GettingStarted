5e8afa8bc490b28678396667622fc491f54d4cdb
Jayanth Chornur Srinivasa <chornur.srinivasa.jayanth@volvo.com>
2017-06-14 17:56:40 +0530

    added routes for books authors . Added list of Books.

--- app.js
+++ app.js
@@ -28,9 +28,41 @@ app.set('view engine','.hbs');
 
 app.use('/Books', bookRouter);
 
+var books = [{
+  title :"War and Peace",
+  genere :"History / Fiction",
+  author : "Tolopsky",
+  read :"false"
+},{
+  title :"Harry Potter",
+  genere :"Fantasy",
+  author : "J K Rowling",
+  read :"true"
+},{
+  title :"The Hobbit",
+  genere :"Fantasy Drama",
+  author : "James T Kirk",
+  read :"false"
+},{
+  title :"Game of thrones",
+  genere :"Drama",
+  author : "Crowne",
+  read :"false"
+}];
+
 bookRouter.route('/')
 .get(function (res, res) {
-  res.send("Hello Books");
+  res.render('books', {
+    title : "Hello from Render",
+    books : books,
+    nav :[{
+      Link : "/Author",
+      Text : "Authors"
+    },{
+      Link : "/Books",
+      Text : "Books"
+    }]
+  });
 });
 
 bookRouter.route('/single')
