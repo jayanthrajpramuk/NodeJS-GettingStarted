121c3424e44a11500e7f986e613db2f093ae1888
Jayanth Chornur Srinivasa <chornur.srinivasa.jayanth@volvo.com>
2017-07-01 12:38:16 +0530

    Mocha test Framework and NOtes about NodeJS patterns

---
+++ test/tests.js
@@ -0,0 +1,17 @@
+/**
+ * Created by a028515 on 01-07-2017.
+ */
+var assert = require('assert');
+
+describe("first Test", function () {
+  describe("Check Number is 9", function () {
+    it("is true", function () {
+          var a = 9;
+          if(a == 9){
+            assert.ok(true);
+          }
+    })
+    
+  })
+
+});
