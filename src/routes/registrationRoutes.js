119b24c707f7ae996ebd85fe097959cbf03c0175
Jayanth Chornur Srinivasa <chornur.srinivasa.jayanth@volvo.com>
2018-02-06 10:22:20 +0530

    Insert statement added

--- src/routes/registrationRoutes.js
+++ src/routes/registrationRoutes.js
@@ -47,6 +47,23 @@ var routes = function(nav) {
 
   });
 
+
+  registrationRouter.route('/save').post(function (req, res) {
+
+    //TODO : to get values from the client.
+
+    pool.query('INSERT INTO paientregistration(paientid, fname, mnane, lname, created_on, last_login) values($1, $2, $3, $4, $5, $6)', [1,'J','R','P',null,null] , function(err, resultSet) {
+      if(err) {
+        return console.error('error running query', err);
+      }
+
+      console.log('Data Saved in DB', resultSet);
+
+    });
+
+  });
+
+
   return registrationRouter;
 };
 
